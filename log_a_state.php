e820ac5260616c8af9b47295c0eae970f1ffaf4c
Rob Clarkson <rob.clarkson@gmail.com>
2016-07-06 11:49:50 -0400

    cleanup up nav a bit

--- log_a_state.php
+++ log_a_state.php
@@ -119,97 +119,40 @@
 //echo ("var sublg = '$sublg';\n");
 //echo '</script>';
 
-$upl = 'Choose file: <input type="file" name="uploadfile" />';		
+			$upl = 'Choose file: <input type="file" name="uploadfile" />';		
 			$pwd = 'Enter Password: <input name="pwd" type="password" size="20" maxlength="20">';
 			$loggame = '<input type="submit" name="submit" value="Upload" />';
 ?>
+<link href="css/nhl94.css" rel="stylesheet" type="text/css" />
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>NHL94 Online.com | Log A State</title>
-<!--<link href="/css/main.css" rel="stylesheet" type="text/css" /> -->
-<!-- <script language="JavaScript">dqm__codebase = "/jslib/nav_bar/"</script>
-<script language="JavaScript" src="/jslib/nav_bar/nav_bar.js"></script>
-<script language="JavaScript" src="/jslib/nav_bar/tdqm_loader.js"></script> -->
 </head>
 
 <body>
-<?php //include($_SERVER['DOCUMENT_ROOT']. "/html/header.php"); ?>
-<table width="100%" border="0" cellpadding="5" cellspacing="0" bgcolor="#FFFFFF">
-  <tr>  
-    <td valign="top"><table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" class="border_table1">
-      <tr>
-        <td height="45" valign="middle"><div align="center"><img src="http://www.nhl94.com/images/artwork/nhl94_cartridge.gif" alt="Log A Game" width="150" height="159" /></div></td>
-      </tr>
-      <tr>
-        <td valign="top" background="/images/2007/ea-sports-bg.gif"><div align="left">
-          <table width="100%" border="0" cellspacing="0" cellpadding="3">
+
+<div class="main">  
+				<a href="index.php"><img src="http://www.nhl94.com/images/artwork/nhl94_cartridge.gif" alt="Log A Game" width="150" height="159" /></a>
+        
+        <p>
+            <?= $msg ?>
+        </p>
+
+        <p><u>Upload Game</u>        
             
-            <tr>
-              <td height="50" valign="top"><div align="center">               
-                
-		  <?php //if(LgActive($lg)){ ?>
-     
-	        <p class="small_bright_blue_bold"></p>
-              </div></td>
-            </tr>
-          </table>
-          <table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-            <tr>
-              <td valign="top"><div align="center">
-                  
-              </div></td>
-            </tr>
-            <tr>
-              <td align="center"><p style="font-size:125%" class="bold_white"><?= $msg ?></p>
-                  <form method="post" action="save_state_new.php" enctype="multipart/form-data">
-                    <?//= $hiddenfields ?>
-                    <table width="300" border="0" align="center" cellpadding="3" cellspacing="0" class="border_table2">
-                      <tr>
-                        <td align="center" colspan="3" class="small_black_bold" background="/images/2007/menu-bg-fade.gif" style="font-size:125%"></td>
-                      </tr>
-                      <tr>
-                        <td align="center"><?//= $bigaway ?></td>
-                        <td align="center" width="20"></td>
-                        <td align="center"><?//= $bighome ?></td>
-                      </tr>
-                      <tr>
-                      	<td align="center"><span style="font-size:125%" class="heading_black"><?//=$awayteam ?>
-                        </span></td>
-                        <td width="20"></td>
-                        <td align="center"><span style="font-size:125%" class="heading_black"><?//=$hometeam ?>
-                        </span></td>
-                      </tr>  
-                    </table><br />
-                    <table width="580" border="0" align="center" cellpadding="3" cellspacing="0" bordercolor="#000000" class="border_table2">
-                      <tr>
-                      	<input type="hidden" name="MAX_FILE_SIZE" value="400000" />
-                      	<td colspan="4" align="center"><?= $upl ?></td>
-                      </tr>  
-                      <tr>
-												<input type="hidden" name="userid" value="2" />                      	
-                        <td align="center" bgcolor="#FFFFFF"><input type="hidden" name="seriesid" value="7" /></td>
-                        <td align="center" bgcolor="#FFFFFF"><?= $pwd ?>&nbsp;&nbsp;<?= $loggame ?></td>                        
-                      </tr>
-                    </table>
-                  </form>
-		
-              </td>
-            </tr>
-          </table>
-          </div></td>
-      </tr>
-    </table>
-    <table width="100%" border="0" cellpadding="8" cellspacing="0" class="border_table1">
-      <tr>
-        <td><?php //include($_SERVER['DOCUMENT_ROOT']. "/html/feedback.php"); ?></td>
-      </tr>
-    </table></td>
-  </tr>
-</table>
-<?php //include($_SERVER['DOCUMENT_ROOT']. "/html/footer.php"); ?>
+						<form method="post" action="save_state_new.php" enctype="multipart/form-data">
+								<input type="hidden" name="MAX_FILE_SIZE" value="400000" />
+								<input type="hidden" name="userid" value="2" />  
+						 		<input type="hidden" name="seriesid" value="7" />
+								 <?= $upl ?><br/>
+                <?= $pwd ?>&nbsp;&nbsp;<?= $loggame ?></br>                      
+            </form>
+
+        </p>
+</div>       
 </body>
 </html>
 <?php
     /* Close SQL-connection */
-    //mysql_close();
+    //mysqli_close($conn);
 ?>
