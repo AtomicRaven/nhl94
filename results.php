8aa4533c5cf644a7616cdab2ad8e163d60aa7e65
jimbobmcqueen <matt@mattgarvin.com>
2016-08-06 02:04:13 -0400

    trimmed tablr

--- results.php
+++ results.php
@@ -25,33 +25,27 @@
 					<table class="standard">
 						<tr class="heading">
 							<td class="c"><span class="note">series</span><br />#</td>
-							<td class="c" colspan="3">Teams</td>
+							<td class="c">Teams</td>
 							<td class="c">Status / Update</td>
 							<td class="">&nbsp;</td>
 						</tr>						
 						<tr class="stripe">
 							<td class="c">101</td>
-							<td class="">MTL</td>
-							<td class="">vs</td>
-							<td class="">BOS</td>
+							<td class="c">MTL<br/>vs<br/>BOS</td>
 							<td class="stanley">MTL wins <nobr>in 7</nobr><br /> 
 								<span class="note">Updated Aug 01, 2016 @ 6:30pm</span></td>
 							<td class=""><button type="button" class="square" onclick="location.href='resultsSeries.php'">Details</button></td>
 						</tr>
 						<tr class="">
 							<td class="c">100</td>
-							<td class="">TOR</td>
-							<td class="">vs</td>
-							<td class="">WPG</td>
+							<td class="c">TOR<br/>vs<br/>WPG</td>
 							<td class="">In progress <nobr>(4 gms)</nobr><br /> 
 								<span class="note">Updated Aug 01, 2016 @ 6:30pm</span></td>
 							<td class=""><button type="button" class="square" onclick="location.href='resultsSeries.php'">Details</button></td>
 						</tr>
 						<tr class="stripe">
 							<td class="c">99</td>
-							<td class="">BOS</td>
-							<td class="">vs</td>
-							<td class="">MTL</td>
+							<td class="c">BOS<br/>vs<br/>MTL</td>
 							<td class="stanley">BOS wins <nobr>in 6</nobr><br /> 
 								<span class="note">Updated Aug 01, 2016 @ 6:30pm</span></td>
 							<td class=""><button type="button" class="square" onclick="location.href='resultsSeries.php'">Details</button></td>
