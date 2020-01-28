cf96910991c58801490bde1b28a15dfd8494731c
Rob Clarkson <rob.clarkson@gmail.com>
2020-01-28 12:51:53 -0500

    Update config.php

--- _INCLUDES/config.php
+++ _INCLUDES/config.php
@@ -12,21 +12,4 @@ $password = "";
 $dbname = "nhl94db";
 $dumppath = "C:/xampp/mysql/bin/";
 
-
-//prod
-/*
-$dbname = "nhl94db-31313577c2";
-$servername = "shareddb-p.hosting.stackcp.net";
-$username = "nhl94";
-$password = "Mysp@ce2174";
-$dumppath = "/usr/bin/mysqldump";
-*/
-
-// matt
-/*
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "nhl94db";
-*/
 ?>
